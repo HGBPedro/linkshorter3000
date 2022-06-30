37edc3e046d187c08a91b5bd76697a8eeae16628
Pedro H <pedro.bonel@gmail.com>
2022-06-29 23:25:36 -0300

    Refactored home page, now it is as simple as possible

--- frontend/linkshorter3000/src/Assets/index.ts
+++ frontend/linkshorter3000/src/Assets/index.ts
@@ -3,18 +3,12 @@ import LinksBlob2 from './bg-blob-links-2.svg'
 import animatedChain from './animatedChain.json'
 import rightArrow from './right-arrow.svg'
 import copyIcon from './copy-icon.svg'
-import IndexBlob1 from './bg-blob-index-1.svg'
-import IndexBlob2 from './bg-blob-index-2.svg'
-import computer from './computer.svg'
 
 export {
   LinksBlob1,
   LinksBlob2,
   animatedChain,
   rightArrow,
-  IndexBlob1,
   copyIcon,
-  IndexBlob2,
-  computer
 }
 
--- frontend/linkshorter3000/src/Views/Home.tsx
+++ frontend/linkshorter3000/src/Views/Home.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import indexImage from 'Assets/indexImage.png'
 
 function Home() {
   return (
     <div className='home'>
-      <img src={indexImage} alt="home logo" className='home__logo' />
       <div className='home__container'>
-        <h2 className='header'>Linkshorter 3000</h2>
-        <p>The best link shortener around</p>
+        <h2 className='text header'>Linkshorter 3000</h2>
+        <p className='text'>The best link shortener around</p>
         <div className='home__button-group'>
           <a href='links' className='home__button'>Get started</a>
           <a href='about' className='home__button'>About</a>
